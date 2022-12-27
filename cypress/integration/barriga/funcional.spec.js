5678f6a534a2de3460d6981570e671950f52649f
Gustavo Lourenço <gustavo.lourenco@dbccompany.com.br>
2022-12-27 14:39:54 -0300

    - Refatora testes
    Inclui novos locators
    Altera a estratégia de gerenciamento de massa
    Cria teste de verificacao de saldo
    Cria de eliminar movimentacao

--- cypress/integration/barriga/funcional.spec.js
+++ cypress/integration/barriga/funcional.spec.js
@@ -6,6 +6,10 @@ import '../../support/commandsContas'
 describe('Testes funcionais de uma aplicação real', () => {
     before(() => {
         cy.login('gl@gl.com', '131294')
+    })
+
+    beforeEach(() => {
+        cy.get(loc.MENU.HOME).click()
         cy.resetApp()
     })
 
@@ -15,12 +19,12 @@ describe('Testes funcionais de uma aplicação real', () => {
 
     it('Inserir Conta', () => {
         cy.acessarMenuConta()
-        cy.inserirConta('Conta Teste 6')
+        cy.inserirConta('Conta Teste')
     })
 
     it('Alterar conta', () => {
         cy.acessarMenuConta()
-        cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click()
+        cy.xpath(loc.CONTAS.FN_XP_BTN_ALTERAR('Conta para alterar')).click()
         cy.get(loc.CONTAS.NOME).clear().type('Conta Alterada')
         cy.get(loc.CONTAS.BTN_SALVAR).click()
         cy.xpath(loc.MESSAGE).should('contain', 'Conta atualizada com sucesso!')
@@ -28,16 +32,43 @@ describe('Testes funcionais de uma aplicação real', () => {
 
     it('Inserir Conta Repetida', () => {
         cy.acessarMenuConta()
-        cy.inserirConta('Conta Alterada')
+        cy.inserirConta('Conta mesmo nome')
     })
 
-    it.only('Adicionar Movimentacao', () => {
+    it('Adicionar Movimentacao', () => {
         cy.get(loc.MOVIMENTACAO.MOVIMENTACAO).click()
         cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Teste')
-        cy.get(loc.MOVIMENTACAO.VALOR).type('100,00')
+        cy.get(loc.MOVIMENTACAO.VALOR).type('1001,00')
         cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Gustavo')
+        cy.get(loc.MOVIMENTACAO.CONTA).select('Conta para movimentacoes')
+        cy.get(loc.MOVIMENTACAO.BTN_CONTA_PAGA).click()
         cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
         cy.xpath(loc.MESSAGE).should('contain', 'Movimentação inserida com sucesso')
+    })
+
+    it('Verifica o saldo', () => {
+        cy.get(loc.MENU.HOME).click()
+        cy.xpath(loc.HOME.FN_XP_CONTA('Conta para saldo')).should('contain', 'Conta para saldo')
+        cy.xpath(loc.HOME.FN_XP_SALDO('Conta para saldo', '534,00')).should('contain', '534,00')
+        
+
+
+        cy.get(loc.MENU.EXTRATO).click()
+        
+
+        cy.xpath(loc.EXTRATO.FN_XP_BTN_EDITAR('Movimentacao 1, calculo saldo')).click()
+        cy.get(loc.MOVIMENTACAO.DESCRICAO).should('have.value', 'Movimentacao 1, calculo saldo')
+
+        cy.get(loc.MOVIMENTACAO.BTN_CONTA_PAGA).click()
+        cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
+        cy.xpath(loc.MESSAGE).should('contain', 'Movimentação alterada com sucesso')
+    })
+
+    it('Elimina uma movimentação', () => {
+        cy.get(loc.MENU.EXTRATO).click()
+        cy.xpath(loc.EXTRATO.FN_XP_BTN_EXCLUIR('Movimentacao para exclusao')).click()
+        cy.xpath(loc.MESSAGE).should('contain', 'Movimentação removida com sucesso')
+
     })
 
 })
--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -7,21 +7,33 @@ const locators = {
     MENU: {
         SETTING: '[data-test=menu-settings]',
         CONTAS: '[href="/contas"]',
-        RESET:'[href="/reset"]'
+        RESET:'[href="/reset"]',
+        HOME:'[data-test=menu-home]',
+        EXTRATO:'[data-test=menu-extrato]'
     },
     CONTAS: {
         NOME: '[data-test=nome]',
         BTN_SALVAR: '.btn',
-        XP_BTN_ALTERAR: "//table[@class='table']//tr[contains(. , 'Conta Teste 6')]//i[@class='far fa-edit']",
+        FN_XP_BTN_ALTERAR: (conta) => `//table[@class='table']//tr[contains(. , '${conta}')]//i[@class='far fa-edit']`,
     },
     MOVIMENTACAO: {
         MOVIMENTACAO: '[data-test=menu-movimentacao]',
         DESCRICAO: '[data-test=descricao]',
         INTERESSADO: '[data-test=envolvido]',
         VALOR: '[data-test=valor]',
-        BTN_SALVAR: '.btn-primary'
+        CONTA: '[data-test=conta]',
+        BTN_SALVAR: '.btn-primary',
+        BTN_CONTA_PAGA: '[data-test=status]'
     },
-    MESSAGE: '//div[@id=\'toast-container\']'
+    MESSAGE: '//div[@id=\'toast-container\']',
+    HOME: {
+        FN_XP_CONTA: nome => `//table[@class='table table-hover table-bordered']//td[contains(., '${nome}')]`,
+        FN_XP_SALDO: (nome, value) => `//table[@class='table table-hover table-bordered']//td[contains(., '${nome}')]/..//td[contains(., '${value}')]`
+    }, 
+    EXTRATO: {
+        FN_XP_BTN_EXCLUIR: (conta) =>  `//div[@class='list-group']//span[contains(., '${conta}')]/../../..//div[@class='col col-md-1']//i[@class='far fa-trash-alt']`,
+        FN_XP_BTN_EDITAR: (conta) =>   `//div[@class='list-group']//span[contains(., '${conta}')]/../../..//div[@class='col col-md-1']//i[@class='fas fa-edit']`
+    }
 }
 
 export default locators;
